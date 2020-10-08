1a67638fa471d92ef54b0443487b7ca449971f0f
SRagavendar <ragajks@gmail.com>
2020-10-08 22:16:19 +0530

    Updated scraper

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,7 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
- def get_google_page(name):
+def get_google_page(name):
  	query = name.replace(' ', '+')
  	URL = f"https://google/com/search?q={query}+careers"
  	resp = requests.get(URL)
@@ -15,6 +15,17 @@ from bs4 import BeautifulSoup
  		for a in soup.find_all('a', href=True):
  			if 'url' in a['href']:
  				url_list = a['href'][7:].split('/')
- 				
+ 				if name in url_list[2]:
+ 					return '/'.join(url_fixer(url_list))
+ 					break
+def url_fixer(url_list):
+	if len(url_list[-1]) > 8:
+		url_list[-1] = url_list[-1].split('&')[0]
+	return url_list
+
 if __name__ == '__main__':
-	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("name", help="name of the company", type=str)
+	args = parser.parse_args()
+	url - get_google_page(args.name)
+	print(url)
